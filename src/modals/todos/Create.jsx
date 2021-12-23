9f7d717110a8241eb73f8a2596a9c4f5c885f404
pdchung <pdchung18@gmail.com>
2021-12-23 15:42:32 +0700

    reuse modal for Edit function

--- src/modals/todos/Create.jsx
+++ src/modals/todos/Create.jsx
@@ -5,31 +5,26 @@ import Modal from 'react-bootstrap/Modal'
 import FormsTodosChange from '@/forms/todos/Change'
 
 const initialValues = {
-  title: '',
-  TodoItems: [
-    {
-      name: '',
-      checked: false
-    }
-  ]
+  title: ''
 }
 
-const ModalsTodosCreate = ({ close, onSubmit }) => (
+const ModalsTodosCreate = ({ close, onSubmit, title }) => (
   <Modal show onHide={close}>
     <Modal.Header closeButton>
       <Modal.Title>Create Todo</Modal.Title>
     </Modal.Header>
     <Modal.Body>
       <FormsTodosChange
-        initialValues={initialValues}
+        initialValues={!!title ? { title } : initialValues}
         onSubmit={onSubmit}
       />
     </Modal.Body>
   </Modal>
-)
+);
 ModalsTodosCreate.propTypes = {
   close: PropTypes.func.isRequired,
-  onSubmit: PropTypes.func.isRequired
+  onSubmit: PropTypes.func.isRequired,
+  title: PropTypes.string
 }
 
 export default ModalsTodosCreate
